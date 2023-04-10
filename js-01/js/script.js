2dedf4d1261057e1662b74927fae5a1579927a2d
Jean Paul Ferreira <jeanpaul@jepafe.net>
2023-04-10 12:09:36 -0300

    12. Javascript - Introducción

---
+++ js-01/js/script.js
@@ -0,0 +1,20 @@
+// alert("Hola JS");
+// console.log("Hola JS...");
+
+let nombre;
+
+nombre = prompt("Ingrese su nombre");
+
+alert(nombre);
+
+console.log(nombre);
+
+nombre = "Otro nombre";
+
+console.log(nombre);
+
+const PI = 3.1416;
+
+const DOS_PALABRAS = "Dos palabras";
+
+const apellido = "Perez";
